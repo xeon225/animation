03dcd52bfd2003467fa3ad83ed6355c24132a654
王磊 <11169514@qq.com>
2019-04-11 11:29:18 +0800

    0411

--- src/components/animation/ps4.vue
+++ src/components/animation/ps4.vue
@@ -2,7 +2,7 @@
   <div id="ps4">
   	<div class="pos-r text-darker" :style="'width:'+Width+'px;height:'+Height+'px;'">
   		<div v-for="items in path">
-  			<div v-for="(item,$index) in items.item" v-if="$index + 1 < items.item.length" class="an_1" :class="item.l ? 'linearGradientTopRight' : 'linearGradientLeftTop'" :style="pathLine(item,items.item[$index+1]) + durationDelay($index,items.item.length)"></div>
+  			<div v-for="(item,$index) in items.item" v-if="$index + 1 < items.item.length" class="an_1" :class="linearGradient(item,items.item[$index+1]) ? 'linearGradientTopRight' : 'linearGradientLeftTop'" :style="pathLine(item,items.item[$index+1]) + durationDelay($index,items.item.length)"></div>
   		</div>
   	</div>
   </div>
@@ -18,156 +18,156 @@ export default {
 	    return {
 	    	Width:900,
 	    	Height:500,
-	    	path:[			//从svg 提取path线性路径坐标
+	    	path:[			//从svg 提取path线性路径坐标    l 线性渐变的方向 false从左下到向上，true 从左上到右下 
 	    		{
 	    			item:[
-		    			{x:563.9, y:361.4, l:false ,o:'bottom left'},
-		    			{x:582, y:303.3, l:false ,o:'bottom left'},
-		    			{x:892.9, y:103.1, l:false ,o:'top right'},
-		    			{x:876.4, y:156.5, l:false ,o:'top right'},
-		    			{x:563.9, y:361.4, l:false ,o:'top right'}
+		    			{x:563.9, y:361.4 ,o:'bottom left'},
+		    			{x:582, y:303.3 ,o:'bottom left'},
+		    			{x:892.9, y:103.1 ,o:'top right'},
+		    			{x:876.4, y:156.5 ,o:'top right'},
+		    			{x:563.9, y:361.4 ,o:'top right'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:892.9, y:103.1, l:true ,o:'bottom right'},
-		    			{x:573.4, y:40.3, l:false ,o:'top right'},
-		    			{x:571.8, y:42.7, l:true ,o:'bottom right'},
-		    			{x:568.1, y:41.7, l:false ,o:'bottom left'},
-		    			{x:569.7, y:38.7, l:true ,o:'bottom right'},
-		    			{x:388.1, y:1, l:false ,o:'top right'},
-		    			{x:41.8, y:162.8, l:true ,o:'top left'},
-		    			{x:235.7, y:212.2, l:true ,o:'top left'},
-		    			{x:582, y:303.3, l:false ,o:'bottom left'}
+		    			{x:892.9, y:103.1 ,o:'bottom right'},
+		    			{x:573.4, y:40.3 ,o:'top right'},
+		    			{x:571.8, y:42.7 ,o:'bottom right'},
+		    			{x:568.1, y:41.7 ,o:'bottom left'},
+		    			{x:569.7, y:38.7 ,o:'bottom right'},
+		    			{x:388.1, y:1 ,o:'top right'},
+		    			{x:41.8, y:162.8 ,o:'top left'},
+		    			{x:235.7, y:212.2 ,o:'top left'},
+		    			{x:582, y:303.3 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:225.8, y:271.4, l:false ,o:'bottom left'},
-		    			{x:241.7, y:213.8, l:false ,o:'bottom left'},
-		    			{x:573.4, y:40.3, l:true ,o:'bottom left'}
+		    			{x:225.8, y:271.4 ,o:'bottom left'},
+		    			{x:241.7, y:213.8 ,o:'bottom left'},
+		    			{x:573.4, y:40.3 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:221.1, y:269.8, l:false ,o:'bottom left'},
-		    			{x:237.7, y:212.8, l:false ,o:'bottom left'},
-		    			{x:569.7, y:38.7, l:true ,o:'bottom left'}
+		    			{x:221.1, y:269.8 ,o:'bottom left'},
+		    			{x:237.7, y:212.8 ,o:'bottom left'},
+		    			{x:569.7, y:38.7 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:41.8, y:162.8, l:false ,o:'top right'},
-		    			{x:23.7, y:217.5, l:true ,o:'top left'},
-		    			{x:563.9, y:361.4, l:true ,o:'top left'}
+		    			{x:41.8, y:162.8 ,o:'top right'},
+		    			{x:23.7, y:217.5 ,o:'top left'},
+		    			{x:563.9, y:361.4 ,o:'top left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:51.4, y:224.8, l:false ,o:'top left'},
-		    			{x:51.4, y:235.8, l:true ,o:'top left'},
-		    			{x:313.6, y:306.9, l:true ,o:'top left'}
+		    			{x:51.4, y:224.8 ,o:'top left'},
+		    			{x:51.4, y:235.8 ,o:'top left'},
+		    			{x:313.6, y:306.9 ,o:'top left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:848.9, y:175.1, l:true ,o:'top left'},
-		    			{x:869.1, y:178.7, l:false ,o:'top right'},
-		    			{x:855, y:232.3, l:false ,o:'top right'},
-		    			{x:537.8, y:447.3, l:true ,o:'right bottom'},
-		    			{x:407.9, y:410.3, l:true ,o:'right bottom'},
-		    			{x:222.6, y:357.6, l:true ,o:'right bottom'},
-		    			{x:201.2, y:351.5, l:false ,o:'top left'},
-		    			{x:202.8, y:346.4, l:false ,o:'top right'},
-		    			{x:196, y:351.5, l:true ,o:'right bottom'},
-		    			{x:1.2, y:297, l:false ,o:'bottom left'},
-		    			{x:16.9, y:242.6, l:false ,o:'bottom left'},
-		    			{x:48.9, y:224.8, l:false ,o:'top right'}
+		    			{x:848.9, y:175.1 ,o:'top left'},
+		    			{x:869.1, y:178.7 ,o:'top right'},
+		    			{x:855, y:232.3 ,o:'top right'},
+		    			{x:537.8, y:447.3 ,o:'right bottom'},
+		    			{x:407.9, y:410.3 ,o:'right bottom'},
+		    			{x:222.6, y:357.6 ,o:'right bottom'},
+		    			{x:201.2, y:351.5 ,o:'top left'},
+		    			{x:202.8, y:346.4 ,o:'top right'},
+		    			{x:196, y:351.5 ,o:'right bottom'},
+		    			{x:1.2, y:297 ,o:'bottom left'},
+		    			{x:16.9, y:242.6 ,o:'bottom left'},
+		    			{x:48.9, y:224.8 ,o:'top right'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:16.9, y:242.6, l:true ,o:'bototm left'},
-		    			{x:555, y:388.6, l:false ,o:'bottom left'},
-		    			{x:869.1, y:178.7, l:true ,o:'bototm left'}
+		    			{x:16.9, y:242.6 ,o:'bototm left'},
+		    			{x:555, y:388.6 ,o:'bottom left'},
+		    			{x:869.1, y:178.7 ,o:'bototm left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:547, y:356.9, l:false ,o:'top right'},
-		    			{x:547, y:369.8, l:true ,o:'right bottom'},
-		    			{x:395.4, y:330.7, l:true ,o:'right bottom'}
+		    			{x:547, y:356.9 ,o:'top right'},
+		    			{x:547, y:369.8 ,o:'right bottom'},
+		    			{x:395.4, y:330.7 ,o:'right bottom'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:196, y:351.5, l:false ,o:'bottom left'},
-		    			{x:211.8, y:295.5, l:true ,o:'bottom left'}
+		    			{x:196, y:351.5 ,o:'bottom left'},
+		    			{x:211.8, y:295.5 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:201.2, y:351.5, l:false ,o:'bottom left'},
-		    			{x:218.1, y:297.4, l:true ,o:'bottom left'}
+		    			{x:201.2, y:351.5 ,o:'bottom left'},
+		    			{x:218.1, y:297.4 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:323.9, y:300.4, l:false ,o:'top right'},
-		    			{x:323.9, y:311.2, l:true ,o:'left top'},
-		    			{x:345.5, y:316.7, l:true ,o:'bottom left'},
-		    			{x:345.5, y:305.7, l:true ,o:'right bottom'},
-		    			{x:323.9, y:300.4, l:false ,o:'right bottom'}
+		    			{x:323.9, y:300.4 ,o:'top right'},
+		    			{x:323.9, y:311.2 ,o:'left top'},
+		    			{x:345.5, y:316.7 ,o:'bottom left'},
+		    			{x:345.5, y:305.7 ,o:'right bottom'},
+		    			{x:323.9, y:300.4 ,o:'right bottom'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:326, y:308.6, l:true ,o:'left top'},
-		    			{x:343.3, y:312.5, l:true ,o:'left top'}
+		    			{x:326, y:308.6 ,o:'left top'},
+		    			{x:343.3, y:312.5 ,o:'left top'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:370.2, y:313.1, l:false ,o:'top right'},
-		    			{x:370.2, y:323.8, l:true ,o:'left top'},
-		    			{x:391.8, y:329.3, l:true ,o:'bottom left'},
-		    			{x:391.8, y:318.3, l:true ,o:'right bottom'},
-		    			{x:370.2, y:313.1, l:false ,o:'right bottom'}
+		    			{x:370.2, y:313.1 ,o:'top right'},
+		    			{x:370.2, y:323.8 ,o:'left top'},
+		    			{x:391.8, y:329.3 ,o:'bottom left'},
+		    			{x:391.8, y:318.3 ,o:'right bottom'},
+		    			{x:370.2, y:313.1 ,o:'right bottom'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:372.3, y:321.2, l:true ,o:'left top'},
-		    			{x:389.6, y:325.1, l:true ,o:'left top'}
+		    			{x:372.3, y:321.2 ,o:'left top'},
+		    			{x:389.6, y:325.1 ,o:'left top'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:477.6, y:297.2, l:true ,o:'left top'},
-		    			{x:491.6, y:301.8, l:false ,o:'top right'},
-		    			{x:487.6, y:308.2, l:true ,o:'bottom right'},
-		    			{x:474.6, y:304.2, l:false ,o:'top right'},
-		    			{x:470.6, y:312.2, l:false ,o:'bottom left'}
+		    			{x:477.6, y:297.2 ,o:'left top'},
+		    			{x:491.6, y:301.8 ,o:'top right'},
+		    			{x:487.6, y:308.2 ,o:'bottom right'},
+		    			{x:474.6, y:304.2 ,o:'top right'},
+		    			{x:470.6, y:312.2 ,o:'bottom left'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:519.4, y:310.2, l:true ,o:'top right'},
-		    			{x:510.4, y:306.8, l:false ,o:'top right'},
-		    			{x:502.6, y:321.2, l:true ,o:'top right'},
-		    			{x:493.6, y:318.2, l:false ,o:'left top'}
+		    			{x:519.4, y:310.2 ,o:'top right'},
+		    			{x:510.4, y:306.8 ,o:'top right'},
+		    			{x:502.6, y:321.2 ,o:'top right'},
+		    			{x:493.6, y:318.2 ,o:'left top'}
 	             	]
 	    		},
 	    		{
 	    			item:[
-		    			{x:533.2, y:330.7, l:false ,o:'left bottom'},
-		    			{x:538.9, y:315.8, l:false ,o:'right top'},
-		    			{x:519.2, y:322.2, l:true ,o:'top left'},
-		    			{x:538.1, y:328.8, l:false ,o:'top right'}
+		    			{x:533.2, y:330.7 ,o:'left bottom'},
+		    			{x:538.9, y:315.8 ,o:'right top'},
+		    			{x:519.2, y:322.2 ,o:'top left'},
+		    			{x:538.1, y:328.8 ,o:'top right'}
 	             	]
 	    		}
 
-	    		//{x:, l:true},
+	    		//{x:},
 	    	],
 	    	times:5
 	    }
@@ -183,7 +183,7 @@ export default {
                 border = '',
                 origin = c1.o,
                 px = 'px;',
-                path = 'transform: scale(0);'
+                path = ''
 
             //取两个y坐标最小值
             top = 'top:' + Math.min(c1.y,c2.y) + px;
@@ -193,13 +193,14 @@ export default {
             width = 'width:' + Math.abs(c1.x - c2.x) + px;
             //取两个yx坐标间距离
             height = 'height:' + Math.abs(c1.y - c2.y) + px;
-
+            //缩放展开所需要的偏移量
             origin = 'transform-origin:' + origin + ';';
             //判断宽度为0或高度为0 设置border为1px实线
             border = (c1.x == c2.x) ? 'border:1px solid currentColor;' : (c1.y == c2.y) ? 'border:1px solid currentColor;' : '';
-            console.log(c1.o,origin)
+
+            // console.log(c1.l,(c1.x>c2.x && c1.y>c2.y))
             //组成style
-            path += top + right + width + height + border + origin
+            path = top + right + width + height + border + origin
             return path
         },
         durationDelay:function(index,length){
@@ -216,8 +217,14 @@ export default {
             delay = 'animation-duration:' + (this.times / length) + 's;';
 
             style = duration + delay;
-            console.log(style)
             return style
+        },
+        linearGradient:function(c1,c2){
+        	var c1 = c1, 		//坐标起点 x y
+                c2 = c2,		//坐标终点 x y
+                linearGradient = false //线性渐变的方向 false从左下到右上，true 从左上到右下 
+            linearGradient = (c1.x>c2.x === c1.y>c2.y);
+            return linearGradient
         }
     },
 }
@@ -227,7 +234,7 @@ export default {
 <style scoped>
    	.linearGradientLeftTop{
         position: absolute;
-        background: linear-gradient(to left top, transparent calc(50% - 1px), currentColor, transparent calc(50% + 1px));
+        background: linear-gradient(to left top, transparent calc(50% - 2px), currentColor, transparent calc(50% + 2px));
         transform-origin:0 100%;
     }
     .linearGradientTopRight{
@@ -236,6 +243,7 @@ export default {
         transform-origin:0 0;
     }
     .an_1{
+    	transform: scale(0);
     	animation: scale_1 linear;
     	animation-fill-mode: forwards;
     }
