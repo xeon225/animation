67013829ceb143e2753bd0daf8a7e652f985bf52
svgShow <11169514@qq.com>
2019-06-11 10:38:03 +0800

    Update antinomy.vue
    0611

--- src/components/animation/antinomy.vue
+++ src/components/animation/antinomy.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="antinomy">
     <div class="pos-r" style="width:186px;height:200px;">
-      <div class="pos-a paluosi" :style="arrData(item)" v-for="item in dItems">
+      <div class="pos-a paluosi" :style="arrData(item,$index)" v-for="(item,$index) in dItems">
         <div></div>
       </div>
 <!--       <div class="pos-a paluosi" style="z-index: 3">
@@ -46,8 +46,18 @@ export default {
 	    	cx:200,         //圆心x
         cy:200,         //圆心y
         dItems:[
-          {x:1,y:1,z:1},
-          {x:2,y:1,z:1}
+          {x:0,y:0,z:0},
+          {x:1,y:0,z:1},
+          {x:2,y:0,z:0},
+          {x:3,y:0,z:0},
+          {x:4,y:0,z:0},
+          {x:0,y:1,z:0},
+          {x:0,y:2,z:0},
+          {x:0,y:3,z:0},
+          {x:0,y:4,z:0},
+          {x:1,y:2,z:0},
+          {x:2,y:0,z:1},
+          {x:4,y:0,z:-1},
 
         ]
 
@@ -59,83 +69,36 @@ export default {
         // d = d.sort(this.sortNumber);
         // console.log(d.sort(this.sortNumber))
         return d
-      },
-      dataCircle(){
-        let d = this.dataNew,
-            sum = 0,        //总数
-            pi = 0,         //占圆所在角度
-            prevPiSum = 0;  //所在圆起始角度
-
-        d.forEach((item,index)=>{
-          sum += item.item
-        })
-
-        d.forEach((item,index)=>{
-          pi = item.item / sum * this.round;
-          item['pi'] = pi;
-          item['prevPiSum'] = prevPiSum;
-          prevPiSum += pi;
-        })
-        // console.log(d)
-        return d
-      },
-      piN(){
-        return this.dItems.length     //角度数
       }
   	},
 	methods: {
-        arrData:function(d){
-          console.log(d)
-        },
-        cssCircle:function(num,x,y,r){
-            var x = x,
-                y = y,
-                r = r,
-                a = num,
-                aa = a*Math.PI/180,
-                lx = x,
-                ly = y,
-                xar = 0,
-                laf = (a > 180) ? 1 : 0,
-                sf = 1,
-                show = show,
-                sinLx = (lx+(Math.sin(aa)*r))+'px ',    //角弧度终点x
-                cosLy = (ly-(Math.cos(aa)*r))+'px ',    //角弧度终点y
+        arrData:function(d,i){
+          var xl = 50,  //left:50
+              xt = 25,  //top:25
+              xz = 1,
+              yl = 25,  //left:25
+              yt = 50,  //top:50
+              zl = 50, //left:50
+              zt = 25,
+              px = '%;',
+              left = 'left:',
+              top = 'top:',
+              zIndex = 'z-index:',
+              leftN = 0,
+              topN = 0,
+              zIndexN = 9999,
+              style = '';
+          
+          leftN = xl * d.x + zl * d.z;
+          topN = xt * d.x + yt * d.y - zt * d.z;
+          zIndexN = zIndexN + d.x - d.y - d.z;
 
-                center = '50% 50%, ',                     //画布中心点
-                top = '50% 0, ',                          //画布top点
-                topRight = '100% 0, ',                    //画布topRight点
-                right = '100% 50%, ',                     //画布right点
-                rightBottom = '100% 100%, ',              //画布rightBottom点
-                bottom = '50% 100%, ',                    //画布bottom点
-                bottomLeft = '0 100%, ',                  //画布bottomLeft点
-                left = '0 50%, ',                         //画布left点
-                leftTop = '0 0, ',                        //画布leftTop点
-
-
-                dd = center + top,      //图形起始点
-
-                dd_90 = dd + topRight, //小于90度图形起始点
-                dd_180 = dd + topRight + right + rightBottom, //大于90度小于180图形起始点
-                dd_270 = dd + topRight + right + rightBottom + bottom + bottomLeft, //大于180度小于270图形起始点
-                dd_360 = dd + topRight + right + rightBottom + bottom + bottomLeft + left + leftTop; //大于270度小于360图形起始点
-
-                
-                if(a>270){
-                    dd = dd_360;
-                }else if(a>180){
-                    dd = dd_270;
-                }else if(a>90){
-                    dd = dd_180;
-                }else{
-                    dd = dd_90;
-                }
-
-                dd += sinLx + cosLy
-                dd = 'clip-path: polygon('+dd+');'
-
-                // console.log(dd)
-            return dd
+          left = left + leftN + px;
+          top = top+ topN + px;
+          zIndex = zIndex + zIndexN;
+          style = left + top + zIndex;
+          // console.log(style)
+          return style
         }
     }
 }
